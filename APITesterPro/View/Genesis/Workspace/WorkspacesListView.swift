bb0ac61208aa4ed155468de457db50d40bcccae4
Jaseem V V <jaseemvv@icloud.com>
2024-09-28 16:05:17 +0530

    Display workspaces list
    - Display workspaces list for the selected core data store

--- APITesterPro/View/Genesis/Workspace/WorkspacesListView.swift
+++ APITesterPro/View/Genesis/Workspace/WorkspacesListView.swift
@@ -7,6 +7,7 @@
 //
 
 import SwiftUI
+import CoreData
 
 @available(iOS 17.0, *)
 struct WorkspacesListView: View {
@@ -16,10 +17,11 @@ struct WorkspacesListView: View {
     @Environment(\.managedObjectContext) private var ctx
     let db = CoreDataService.shared
     @Environment(\.isLocalStore) var isLocalStore
+    @State private var workspaces: [EWorkspace] = []
 
     var body: some View {
         NavigationView {
-            VStack(alignment: .leading) {
+            VStack(alignment: .center) {
                 Picker("", selection: $selectedWorkspace) {
                     ForEach(section, id: \.self) { elem in
                         Text(" \(elem) ").tag(elem)
@@ -37,7 +39,13 @@ struct WorkspacesListView: View {
                         isLocalStore?.wrappedValue = true
                     }
                 }
-                Spacer()
+                List {
+                    ForEach(workspaces) { ws in
+                        Text(ws.name ?? "")
+                    }
+                }
+                .contentMargins(.top, 8)
+                .listStyle(.plain)
             }
             .navigationTitle("Workspaces")
             .navigationBarTitleDisplayMode(.inline)
@@ -46,7 +54,23 @@ struct WorkspacesListView: View {
             })
             .onAppear {
                 self.selectedWorkspace = (self.isLocalStore?.wrappedValue ?? true) ? "Local" : "iCloud"
+                self.loadWorkspaces()
             }
+            .onChange(of: isLocalStore?.wrappedValue) { oldValue, newValue in
+                self.loadWorkspaces()
+            }
+        }
+    }
+    
+    func loadWorkspaces() {
+        Log.debug("Load workspaces")
+        let fetchRequest: NSFetchRequest<EWorkspace> = EWorkspace.fetchRequest()
+        fetchRequest.sortDescriptors = [NSSortDescriptor(keyPath: \EWorkspace.created, ascending: true)]
+        let moc = isLocalStore?.wrappedValue == true ? db.localMainMOC : db.ckMainMOC
+        do {
+            workspaces = try moc.fetch(fetchRequest)
+        } catch {
+            Log.error("Error fetching workspaces: \(error)")
         }
     }
 }
