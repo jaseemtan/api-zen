ef4cd8e05b9a9328f1cb08a27b9d4dd7a9e9912f
Jaseem V V <jaseemvv@icloud.com>
2025-03-30 12:20:16 +0530

    Initial Commit

---
+++ AZCommon/AZCommon.h
@@ -0,0 +1,18 @@
+//
+//  AZCommon.h
+//  AZCommon
+//
+//  Created by Jaseem V V on 30/03/25.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AZCommon.
+FOUNDATION_EXPORT double AZCommonVersionNumber;
+
+//! Project version string for AZCommon.
+FOUNDATION_EXPORT const unsigned char AZCommonVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AZCommon/PublicHeader.h>
+
+
---
+++ AZCommonTests/AZCommonTests.swift
@@ -0,0 +1,17 @@
+//
+//  AZCommonTests.swift
+//  AZCommonTests
+//
+//  Created by Jaseem V V on 30/03/25.
+//
+
+import Testing
+@testable import AZCommon
+
+struct AZCommonTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
