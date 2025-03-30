461aacad86c42f12bdfe722a3d1ab3643cd942b1
Jaseem V V <jaseemvv@icloud.com>
2025-03-30 12:53:16 +0530

    Initial Commit

---
+++ AZPhone/AZPhone.h
@@ -0,0 +1,18 @@
+//
+//  AZPhone.h
+//  AZPhone
+//
+//  Created by Jaseem V V on 30/03/25.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AZPhone.
+FOUNDATION_EXPORT double AZPhoneVersionNumber;
+
+//! Project version string for AZPhone.
+FOUNDATION_EXPORT const unsigned char AZPhoneVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AZPhone/PublicHeader.h>
+
+
---
+++ AZPhoneTests/AZPhoneTests.swift
@@ -0,0 +1,17 @@
+//
+//  AZPhoneTests.swift
+//  AZPhoneTests
+//
+//  Created by Jaseem V V on 30/03/25.
+//
+
+import Testing
+@testable import AZPhone
+
+struct AZPhoneTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
