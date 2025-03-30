3289b5f68c07c66101c32ad71dde79e014f7a8fa
Jaseem V V <jaseemvv@icloud.com>
2025-03-30 11:49:30 +0530

    Initial Commit

---
+++ AZData/AZData.h
@@ -0,0 +1,18 @@
+//
+//  AZData.h
+//  AZData
+//
+//  Created by Jaseem V V on 30/03/25.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AZData.
+FOUNDATION_EXPORT double AZDataVersionNumber;
+
+//! Project version string for AZData.
+FOUNDATION_EXPORT const unsigned char AZDataVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AZData/PublicHeader.h>
+
+
---
+++ AZDataTests/AZDataTests.swift
@@ -0,0 +1,17 @@
+//
+//  AZDataTests.swift
+//  AZDataTests
+//
+//  Created by Jaseem V V on 30/03/25.
+//
+
+import Testing
+@testable import AZData
+
+struct AZDataTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
