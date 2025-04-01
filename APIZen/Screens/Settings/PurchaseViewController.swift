48ebc74284dd4d7f443dc2ef76e507f2cf3cd2d9
Jaseem V V <jaseemvv@icloud.com>
2025-04-01 22:33:01 +0530

    Purchase screen UI with circuit image at bottom

--- APIZen/Screens/Settings/PurchaseViewController.swift
+++ APIZen/Screens/Settings/PurchaseViewController.swift
@@ -11,8 +11,51 @@ import UIKit
 import AZCommon
 
 class PurchaseTableViewController: UITableViewController {
+    enum CellId: Int {
+        case spacerAfterTop
+        case purchaseStatus
+        case spacerAfterPurchaseStatus
+        case unlockFullVersionButton
+        case spacerAfterUnlockFullVersionButton
+        case trialMessage
+        case freeMessage
+        case fullVersionMessage
+        case spaceAfterFullVersionMessage
+        case circuitImage
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         Log.debug("purchase tvc did load")
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        switch indexPath.row {
+        case CellId.spacerAfterTop.rawValue:
+            return 36
+        case CellId.purchaseStatus.rawValue:
+            return 44
+        case CellId.spacerAfterPurchaseStatus.rawValue:
+            return 24
+        case CellId.unlockFullVersionButton.rawValue:
+            return 44
+        case CellId.spacerAfterUnlockFullVersionButton.rawValue:
+            return 24
+        case CellId.trialMessage.rawValue:
+            return 120  // TODO: get dynamic value
+        case CellId.freeMessage.rawValue:
+            return 0
+        case CellId.fullVersionMessage.rawValue:
+            return 0
+        case CellId.spaceAfterFullVersionMessage.rawValue:
+            let height = UIScreen.main.bounds.height
+            let computedHeight = height - (36 + 44 + 24 + 44 + 24 + 120 + 200 + 100)
+            return computedHeight < 50 ? 50 : computedHeight
+        case CellId.circuitImage.rawValue:
+            return 200
+        default:
+            break
+        }
+        return UITableView.automaticDimension
     }
 }
