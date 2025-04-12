284e416ae3b16075d53622be25a74b0ebc9995df
Jaseem V V <jaseemvv@icloud.com>
2025-04-13 03:23:40 +0530

    IAP state update for FSM

--- APIZen/Libs/StoreKit/IAPStates.swift
+++ APIZen/Libs/StoreKit/IAPStates.swift
@@ -14,32 +14,17 @@ class ProductsFetchState: GKState {
     
 }
 
-/// In this state all products purchased will be fetched and local state updated for each purchase of the product.
-class RestoreAllIAPsState: GKState {
+/// Thanks IAP donation.
+class DonateLowState: GKState {
     
 }
 
-/// In this state a new In-App purchase will be made corresponding to the selected product.
-class PurchaseIAPState: GKState {
+/// Thank you IAP donation.
+class DonateMediumState: GKState {
     
 }
 
-/// This state is entered when after fetching all In-App purchases, the products are found to be cancelled and refunded.
-class IAPCancelledAndRefundedState: GKState {
-    
-}
-
-/// This is the introductory trial state where user can experience the full version of the app for limited period of time.
-class IAPTrialState: GKState {
-    
-}
-
-/// This is the state when trial gets expired and user continues the usage without purchasing the full version. It goes to free mode which has restrictions.
-class FreeState: GKState {
-    
-}
-
-/// This state is entered after user purchases the full version.
-class UnlockedFullVersionState: GKState {
+/// Thank you very much IAP donation.
+class DonateHightState: GKState {
     
 }
