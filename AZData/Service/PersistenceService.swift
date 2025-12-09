20e5cf23f57e20059810325116be3843e766c63b
Jaseem V V <jaseemvv@gmail.com>
2025-12-09 10:56:20 +0530

    Persistence service with business logic
    - Add new workspace incrementing order

---
+++ AZData/Service/PersistenceService.swift
@@ -0,0 +1,25 @@
+//
+//  PersistenceService.swift
+//  AZData
+//
+//  Created by Jaseem V V on 09/12/25.
+//
+
+import CoreData
+import AZCommon
+
+public class PersistenceService {
+    public static let shared = PersistenceService()
+    private lazy var db = { CoreDataService.shared }()
+    
+    // Add new workspace. The workspace order will be incremented.
+    public func createWorkspace(name: String, desc: String, isSyncEnabled: Bool) {
+        let ctx = isSyncEnabled ? self.db.ckMainMOC : self.db.localMainMOC
+        let order = self.db.getOrderOfLastWorkspace(ctx: ctx).inc()
+        Log.debug("Last workspace order: \(order)")
+        if let ws = self.db.createWorkspace(id: self.db.workspaceId(), name: name, desc: desc, isSyncEnabled: isSyncEnabled, ctx: ctx) {
+            ws.order = order
+            self.db.saveMainContext()
+        }
+    }
+}
