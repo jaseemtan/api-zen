c5bdd26a0138aa7852b0664318c695220f02fb1b
Jaseem V V <jaseemvv@gmail.com>
2025-12-09 10:53:02 +0530

    Add new workspace form and functionality
    - Form UI
    - Add new workspace

---
+++ APIZenMac/View/Workspace/AddWorkspaceFormView.swift
@@ -0,0 +1,43 @@
+//
+//  AddWorkspaceFormView.swift
+//  APIZenMac
+//
+//  Created by Jaseem V V on 09/12/25.
+//
+
+import SwiftUI
+import AZCommon
+import AZData
+import CoreData
+
+struct AddWorkspaceFormView: View {
+    @Environment(\.dismiss) private var dismiss
+    @State private var name: String = ""
+    @State private var desc: String = ""
+    @State private var isSyncEnabled: Bool = true  // iCloud syncing enabled by default
+    private let dbSvc = PersistenceService.shared
+    
+    var body: some View {
+        ScrollView {
+            Form {
+                TextField("Name", text: $name)
+                TextField("Description", text: $desc)
+                Toggle("iCloud Sync", isOn: $isSyncEnabled)
+            }
+            .formStyle(.grouped)
+            Button("Save") {
+                saveWorkspace()
+            }
+            .disabled(name.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty)
+            Spacer()
+        }
+        .navigationTitle("New Workspace")
+        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
+    }
+    
+    private func saveWorkspace() {
+        Log.debug("save workspace: \(name) - \(desc) - \(isSyncEnabled)")
+        self.dbSvc.createWorkspace(name: name, desc: desc, isSyncEnabled: isSyncEnabled)
+        dismiss()
+    }
+}
