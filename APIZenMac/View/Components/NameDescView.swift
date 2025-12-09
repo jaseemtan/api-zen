0b6c0f6aa727d998884ddd6c526fe30f2e045dbe
Jaseem V V <jaseemvv@gmail.com>
2025-12-09 14:08:27 +0530

    Add comment

--- APIZenMac/View/Components/NameDescView.swift
+++ APIZenMac/View/Components/NameDescView.swift
@@ -7,6 +7,9 @@
 
 import SwiftUI
 
+// Here we use plain variables instead of @State because this value is obtained from parent and this view does not own this.
+// @State means this view owns this piece of data and is the source of truth for it. SwiftUI will store and preserve it across view reloads. External changes will not reflect once set unless synced in some way.
+// So NameDescView is a pure view of the data. It doesn't own the data.
 struct NameDescView: View {
     var imageName: String
     var name: String
